93f9600e421a0f13a41594a22bd054d314af0bd9
ankit <ankibansal56@gmail.com>
2019-09-16 22:41:46 +0530

    add few handlers

--- src/main/java/io/vertx/blog/first/WikiVerticle.java
+++ src/main/java/io/vertx/blog/first/WikiVerticle.java
@@ -1,5 +1,6 @@
 package io.vertx.blog.first;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +8,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
@@ -17,6 +19,8 @@ import io.vertx.ext.web.handler.BodyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.rjeschke.txtmark.Processor;
+
 public class WikiVerticle extends AbstractVerticle {
 
 	private JDBCClient dbClient;
@@ -25,7 +29,7 @@ public class WikiVerticle extends AbstractVerticle {
 
 	private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
 	private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
-	private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+	private static final String SQL_CREATE_PAGE = "insert into Pages values (4, 'apple', 'apple is good for health')";
 	private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
 	private static final String SQL_ALL_PAGES = "select Name from Pages";
 	private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
@@ -56,6 +60,16 @@ public class WikiVerticle extends AbstractVerticle {
 						promise.complete();
 					}
 				});
+				connection.execute(SQL_CREATE_PAGE, create1 -> {
+					connection.close();
+					if (create1.failed()) {
+						LOGGER.error("Database preparation error", create1.cause());
+						promise.fail(create1.cause());
+					} else {
+						promise.complete();
+					}
+				});
+				
 			}
 		});
 
@@ -123,20 +137,117 @@ public class WikiVerticle extends AbstractVerticle {
 		});
 	}
 
-	private void pageRenderingHandler(RoutingContext context) {
+	private static final String EMPTY_PAGE_MARKDOWN =
+			  "# A new page\n" +
+			    "\n" +
+			    "Feel-free to write in Markdown!\n";
 
+	private void pageRenderingHandler(RoutingContext context) {
+	  String page = context.request().getParam("page"); 
+
+	  dbClient.getConnection(car -> {
+	    if (car.succeeded()) {
+
+	      SQLConnection connection = car.result();
+	      connection.queryWithParams(SQL_GET_PAGE, new JsonArray().add(page), fetch -> {
+	        connection.close();
+	        if (fetch.succeeded()) {
+
+	          JsonArray row = fetch.result().getResults()
+	            .stream()
+	            .findFirst()
+	            .orElseGet(() -> new JsonArray().add(-1).add(EMPTY_PAGE_MARKDOWN));
+	          Integer id = row.getInteger(0);
+	          String rawContent = row.getString(1);
+
+	          context.put("title", page);
+	          context.put("id", id);
+	          context.put("newPage", fetch.result().getResults().size() == 0 ? "yes" : "no");
+	          context.put("rawContent", rawContent);
+	          context.put("content", Processor.process(rawContent));
+	          context.put("timestamp", new Date().toString());
+
+	          templateEngine.render(context.data(), "templates/page.ftl", ar -> {
+	            if (ar.succeeded()) {
+	              context.response().putHeader("Content-Type", "text/html");
+	              context.response().end(ar.result());
+	            } else {
+	              context.fail(ar.cause());
+	            }
+	          });
+	        } else {
+	          context.fail(fetch.cause());
+	        }
+	      });
+
+	    } else {
+	      context.fail(car.cause());
+	    }
+	  });
 	}
 
 	private void pageUpdateHandler(RoutingContext context) {
+		  String id = context.request().getParam("id");
+		  String title = context.request().getParam("title");
+		  String markdown = context.request().getParam("markdown");
+		  boolean newPage = "yes".equals(context.request().getParam("newPage"));
 
-	}
+		  dbClient.getConnection(car -> {
+		    if (car.succeeded()) {
+		      SQLConnection connection = car.result();
+		      String sql = newPage ? SQL_CREATE_PAGE : SQL_SAVE_PAGE;
+		      JsonArray params = new JsonArray();
+		      if (newPage) {
+		        params.add(title).add(markdown);
+		      } else {
+		        params.add(markdown).add(id);
+		      }
+		      connection.updateWithParams(sql, params, res -> {
+		        connection.close();
+		        if (res.succeeded()) {
+		          context.response().setStatusCode(303);
+		          context.response().putHeader("Location", "/wiki/" + title);
+		          context.response().end();
+		        } else {
+		          context.fail(res.cause());
+		        }
+		      });
+		    } else {
+		      context.fail(car.cause());
+		    }
+		  });
+		}
 
 	private void pageCreateHandler(RoutingContext context) {
-
-	}
+		  String pageName = context.request().getParam("name");
+		  String location = "/wiki/" + pageName;
+		  if (pageName == null || pageName.isEmpty()) {
+		    location = "/";
+		  }
+		  context.response().setStatusCode(303);
+		  context.response().putHeader("Location", location);
+		  context.response().end();
+		}
 
 	private void pageDeletionHandler(RoutingContext context) {
-
-	}
+		  String id = context.request().getParam("id");
+		  dbClient.getConnection(car -> {
+		    if (car.succeeded()) {
+		      SQLConnection connection = car.result();
+		      connection.updateWithParams(SQL_DELETE_PAGE, new JsonArray().add(id), res -> {
+		        connection.close();
+		        if (res.succeeded()) {
+		          context.response().setStatusCode(303);
+		          context.response().putHeader("Location", "/");
+		          context.response().end();
+		        } else {
+		          context.fail(res.cause());
+		        }
+		      });
+		    } else {
+		      context.fail(car.cause());
+		    }
+		  });
+		}
 
 }
